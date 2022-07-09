1e318d1bc4c10bbdd12ba360447509bc54a85554
seongwon <syanggu@naver.com>
2022-07-09 21:51:22 +0900

    scaffold, appbar

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: Center(
-        child: Container(width: 50, height: 50, color: Colors.amber),
-      )
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('앱임'),
+
+        ),
+        body: Text('안녕'),
+
+        ),
+
+
+      );
 
   }
 }
