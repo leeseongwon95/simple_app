651f0fb282feae717644a5b3eebfe507188290bd
seongwon <syanggu@naver.com>
2022-07-10 14:59:56 +0900

    ListView builder ListTile State

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+   MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1; // statefulWidget 만들고 class 안에 변수만들면 됨
+  var name = ['아줌마', '아저씨', '할머니'];
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset('free.jpg', width: 150,),
-              Container(
-                width: 300,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('캐논', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                    Text('10분전', style: TextStyle(fontSize: 10, fontWeight: FontWeight.w100),),
-                    Text('0원'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Icon(Icons.favorite),
-                        Text('4')
-                      ],
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: (){
+            setState((){
+              a++;
+            });
+          },
         ),
-      )
+        appBar: AppBar( title: Text('연락처앱'),),
+        body: ListView.builder(
+         itemCount: 3,
+         itemBuilder: (c, i){
+           print(i);
+           return ListTile(
+             leading: Icon(Icons.account_circle_sharp),
+             title: Text(name[i]),
+             );
+           },
+       ),
+      ),
     );
-}
+  }
 }
