ae79565feb029bd1ab25c21d252bb852db6886ec
seongwon <syanggu@naver.com>
2022-07-10 18:38:45 +0900

    state handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+        home:MyApp(),
+      )
+  );
 }
+
 
 class MyApp extends StatefulWidget {
    MyApp({Key? key}) : super(key: key);
@@ -12,41 +17,85 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1; // statefulWidget 만들고 class 안에 변수만들면 됨
+
+  // statefulWidget 만들고 class 안에 변수만들면 됨
   var name = ['아줌마', '아저씨', '할머니'];
   var like = [0, 0, 0];
+  var total = 3;
+
+  addName(a){
+    setState((){
+      name.add(a);
+    });
+  }
+
+  addOne(){
+    setState((){
+      total++;
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         floatingActionButton: FloatingActionButton(
-          child: Text(a.toString()),
-          onPressed: (){
-            setState((){
-              a++;
+          onPressed: () {
+            // showDialog(context)
+            // Scaffold.of(context)
+            // Navigator.pop(context)
+            // Theme.of(context)
+            // context 가 강제됨
+            // MaterialApp이 들어있는 context를 입력해야 잘 동작하는 함수임
+            print(context.findAncestorWidgetOfExactType<MaterialApp>());
+            showDialog(context: context, builder: (context) {
+              return DialogUI(addOne : addOne, addName: addName,);
             });
-          },
+          }
         ),
-        appBar: AppBar( title: Text('연락처앱'),),
+        appBar: AppBar( title: Text(total.toString()),),
         body: ListView.builder(
-         itemCount: 3,
+         itemCount: name.length,
          itemBuilder: (c, i){
-           print(i);
            return ListTile(
+             leading: Icon(Icons.account_circle_sharp),
              title: Text(name[i]),
-             leading: Text(like[i].toString()),
-             trailing:ElevatedButton(
-               child: Text('좋아요'),
-               onPressed: (){
-                 setState((){
-                   like[i]++;
-                 });
-             },),
-
              );
            },
-       ),
+        ),
+    );
+  }
+}
+
+class DialogUI extends StatelessWidget {
+  DialogUI({Key? key, this.addOne, this.addName}) : super(key: key);
+  final addOne;
+  final addName;
+  var inputData = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField( controller: inputData,),
+            TextButton( child: Text('완료'), onPressed: (){
+              addOne();
+              addName(inputData.text);
+            },),
+            TextButton(
+              child: Text('취소'),
+              onPressed: (){
+                Navigator.pop(context);
+                },
+            )
+          ],
+        ),
       ),
     );
   }
